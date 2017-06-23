f504001b9f21fdb3d2fb062cc5373870ef9d3641
realsoc <hugo.djemaa@gmail.com>
2017-06-22 21:08:26 -0300

    Processamento modulable TODO : image name

---
+++ processamento/test.py
@@ -0,0 +1,21 @@
+from DetetorDeProduto import DetetorDeProduto
+
+# YELLOW = 0
+	# GREEN =  1
+	# CLEAR BLUE = 2
+	# RED = 4
+	# DARK BLUE = 6
+	# PURPLE = 7
+dp = DetetorDeProduto()
+print "yellow : ",
+dp.detectColor(0)
+print "green : ",
+dp.detectColor(1)
+print "clear blue : ",
+dp.detectColor(2),
+print "red : ",
+dp.detectColor(4)
+print "dark blue : ",
+dp.detectColor(6)
+print "purple : ",
+dp.detectColor(7)
